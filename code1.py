fed316d19831d4a7376b7ab48d0e015db2117acd
jonydaimary <45644556+jonydaimary@users.noreply.github.com>
2019-01-24 10:59:58 +0530

    Update code1.py

--- code1.py
+++ code1.py
@@ -13,7 +13,6 @@ import datetime
 import requests
 import json
 import aiohttp
-import box
 
 Forbidden= discord.Embed(title="Permission Denied", description="1) Please check whether you have permission to perform this action or not. \n2) Please check whether my role has permission to perform this action in this channel or not. \n3) Please check my role position.", color=0x00ff00)
 client = commands.Bot(description="TEST Official Bot", command_prefix=commands.when_mentioned_or("*"), pm_help = True)

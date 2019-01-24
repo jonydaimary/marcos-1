45b0370aedb20b15a1844d05e09d2599e4c3385c
jonydaimary <45644556+jonydaimary@users.noreply.github.com>
2019-01-24 14:40:31 +0530

    Update code1.py

--- code1.py
+++ code1.py
@@ -35,39 +35,15 @@ async def announce(ctx, channel: discord.Channel=None, *, msg: str):
         await client.send_message(channel, embed=embed)
         await client.delete_message(ctx.message)
 	
-@client.command(pass_context=True)
-async def rps(ctx, *, message=None):
+
+	
+@client.command(pass_context = True)
+async def rolldice(ctx):
+    choices = ['1', '2', '3', '4', '5', '6']
+    color = discord.Color(value=0x00ff00)
+    em = discord.Embed(color=color, title='Rolled! (1 6-sided die)', description=random.choice(choices))
     await client.send_typing(ctx.message.channel)
-    ans = ["rock", "paper", "scissors"]
-    pick=ans[random.randint(0, 2)]
-    embed=discord.Embed(title = "Bot VS {}".format(ctx.message.author.name), color = 0xf9fcfc)
-    embed.set_author(name = ctx.message.author.name, icon_url = ctx.message.author.avatar_url)
-    if message is None:
-        await client.say('```The proper usage is \n!!rps <rock> or <paper> or <scissors>```')
-    if message.lower() != ans[0] and message.lower() != ans[1] and message.lower() != ans[2] :
-        return await client.say("Pick Rock Paper or Scissors")
-    elif message.lower() == pick:
-        embed.add_field(name = "Its a draw!", value = "Bot picked {} too!".format(pick))
-        return await client.say(embed=embed)
-    else:
-        if message.lower()  == "rock" and pick == "paper":
-            embed.add_field(name = "Bot Wins!", value = "Bot picked {}!".format(pick))
-            await client.say(embed=embed)
-        elif message.lower()  == "rock" and pick == "scissors":
-            embed.add_field(name = "{} Wins!".format(ctx.message.author.name), value = "Bot picked {}!".format(pick))
-            await client.say(embed=embed)
-        elif message.lower()  == "paper" and pick == "rock":
-            embed.add_field(name = "{} Wins!".format(ctx.message.author.name), value = "Bot picked {}!".format(pick))
-            await client.say(embed=embed)
-        elif message.lower()  == "paper" and pick == "scissors":
-            embed.add_field(name = "Bot Wins!", value = "Bot picked {}!".format(pick))
-            await client.say(embed=embed)
-        elif message.lower()  == "scissors" and pick == "rock":
-            embed.add_field(name = "Bot Wins!", value = "Bot picked {}!".format(pick))
-            await client.say(embed=embed)
-        else:
-            embed.add_field(name = "{} Wins!".format(ctx.message.author.name), value = "Bot picked {}!".format(pick))
-            await client.say(embed=embed)
+    await client.say(embed=em)
 	     
 
 

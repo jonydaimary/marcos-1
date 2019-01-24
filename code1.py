a5bf4c9ed2bc71290ffbd1650eff9b5923c3b0d6
jonydaimary <45644556+jonydaimary@users.noreply.github.com>
2019-01-24 15:15:18 +0530

    Update code1.py

--- code1.py
+++ code1.py
@@ -36,31 +36,40 @@ async def announce(ctx, channel: discord.Channel=None, *, msg: str):
         await client.delete_message(ctx.message)
 	
 
-	
-@client.command(pass_context = True)
-async def rolldice(ctx):
-    choices = ['1', '2', '3', '4', '5', '6']
-    color = discord.Color(value=0xf9fcfc)
-    em = discord.Embed(color=color, title='Rolled! (1 6-sided die)', description=random.choice(choices))
-    await client.send_typing(ctx.message.channel)
-    await client.say(embed=em)
+@client.command(pass_context=True, aliases=['server'])
+@commands.has_permissions(kick_members=True)
+async def membercount(ctx, *args):
+    """
+    Shows stats and information about current guild.
+    ATTENTION: Please only use this on your own guilds or with explicit
+    permissions of the guilds administrators!
+    """
+    if ctx.message.channel.is_private:
+        await bot.delete_message(ctx.message)
+        return
 
-@client.command(pass_context = True)
-async def flipcoin(ctx):
-    choices = ['Heads', 'Tails', 'Coin self-destructed']
-    color = discord.Color(value=0xf9fcfc)
-    em=discord.Embed(color=color, title='Flipped a coin!')
-    em.description = random.choice(choices)
-    await client.send_typing(ctx.message.channel)
-    await client.say(embed=em)
-	     
+    g = ctx.message.server
 
-@client.command(pass_context=True)
-async def youtube(ctx, *, message: str):
-    new_message = message.replace(" ", "+")
-    url = f"https://www.youtube.com/results?search_query={new_message}"
-    await client.say(url)
-		
+    gid = g.id
+    membs = str(len(g.members))
+    membs_on = str(len([m for m in g.members if not m.status == Status.offline]))
+    users = str(len([m for m in g.members if not m.bot]))
+    users_on = str(len([m for m in g.members if not m.bot and not m.status == Status.offline]))
+    bots = str(len([m for m in g.members if m.bot]))
+    bots_on = str(len([m for m in g.members if m.bot and not m.status == Status.offline]))
+    created = str(g.created_at)
+    
+    em = Embed(title="Membercount")
+    em.description =    "```\n" \
+                        "Members:   %s (%s)\n" \
+                        "  Users:   %s (%s)\n" \
+                        "  Bots:    %s (%s)\n" \
+                        "Created:   %s\n" \
+                        "```" % (membs, membs_on, users, users_on, bots, bots_on, created)
+
+    await client.send_message(ctx.message.channel, embed=em)
+    await client.delete_message(ctx.message)
+
 
 client.run(os.getenv('Token'))
 

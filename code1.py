6d0195170681c6b34e16b1e22e985ce61b1538ab
jonydaimary <45644556+jonydaimary@users.noreply.github.com>
2019-01-24 10:43:32 +0530

    Update code1.py

--- code1.py
+++ code1.py
@@ -28,39 +28,6 @@ async def on_ready():
     print('Created by marcos')
     client.loop.create_task(status_task())
 
-async def req(self, url):
-    res = await self.bot.session.get(f"https://nekos.life/api/v2/img/{url}")
-    res = await res.json()
-    return box.Box(res)
-  
-
-@client.command(pass_context = True)
-async def boobs(self, ctx):
-    """WARNING: NSFW command. Gets pictures of boobs."""
-    if not ctx.channel.nsfw:
-        return await ctx.send("Are you trying to **kill innocent people's eyes**?? I think not!")
-    if not ctx.channel.nsfw:
-        return await ctx.send("Are you trying to **kill innocent people's eyes**?? I think not!")
-
-    res = await self.req("boobs")
-    embed = discord.Embed(color=0Xf9fcfc, title="Boobs :eggplant: ")
-    embed.set_image(url=res.url)
-    embed.set_footer(text=f"Requested by: {str(ctx.author)} | Powered by nekos.life", icon_url=ctx.author.avatar_url)
-    await ctx.send(embed=em)
-
-
-@client.command(pass_context = True)
-async def hentai(self, ctx, is_gif=None):
-    """WARNING: NSFW command. Gets a hentai picture."""
-    if not ctx.channel.nsfw:
-    return await ctx.send("Are you trying to **kill innocent people's eyes**?? I think not!")
-
-    res = await self.req("Random_hentai_gif")
-    em = discord.Embed(color=0Xf9fcfc, title="Hentai :eggplant: ")
-    em.set_image(url=res.url)
-    em.set_footer(text=f"Requested by: {str(ctx.author)} | Powered by nekos.life", icon_url=ctx.author.avatar_url)
-    await ctx.send(embed=em)
-
 
 client.run(os.getenv('Token'))
 

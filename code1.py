5001f84600c85daf28b588b64707adef7adbe308
jonydaimary <45644556+jonydaimary@users.noreply.github.com>
2019-01-24 10:54:54 +0530

    Update code1.py

--- code1.py
+++ code1.py
@@ -16,7 +16,6 @@ import aiohttp
 
 Forbidden= discord.Embed(title="Permission Denied", description="1) Please check whether you have permission to perform this action or not. \n2) Please check whether my role has permission to perform this action in this channel or not. \n3) Please check my role position.", color=0x00ff00)
 client = commands.Bot(description="TEST Official Bot", command_prefix=commands.when_mentioned_or("*"), pm_help = True)
-client.remove_command('help')
 
 @client.event
 async def on_ready():

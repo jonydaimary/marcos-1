b9317f1fb460e6118cef1d9c60349c8663c843d9
jonydaimary <45644556+jonydaimary@users.noreply.github.com>
2019-01-25 11:29:22 +0530

    Update code1.py

--- code1.py
+++ code1.py
@@ -25,48 +25,21 @@ async def on_ready():
 	print('Started pubg') #add_your_bot_name_here
 	return await client.change_presence(game=discord.Game(name='BOT BETA TESTING')) #add_your_bot_status_here
 
+
 @client.command(pass_context = True)
-@commands.has_permissions(administrator=True) 
-async def announce(ctx, channel: discord.Channel=None, *, msg: str):
-    if channel is None:
-        await client.say(" ```Proper usage is\n\nannounce<channel><matter>```")
-    else:
-        embed=discord.Embed(title="Announcement", description="{}".format(msg), color = 0xf9fcfc)
-        await client.send_message(channel, embed=embed)
-        await client.delete_message(ctx.message)
-	
-@client.command(pass_context=True, aliases=['server'])
-@commands.has_permissions(kick_members=True)
-async def membercount(ctx, *args):
-    """
-    Shows stats and information about current guild.
-    ATTENTION: Please only use this on your own guilds or with explicit
-    permissions of the guilds administrators!
-    """
-    if ctx.message.channel.is_private:
-        await bot.delete_message(ctx.message)
+async def announce(ctx, channel: discord.Channel=None, *, msg: str=None):
+    member = ctx.message.author
+    if channel is None or msg is None:
+        await client.say('Invalid args. Use this command like ``mv!announce #channel text here``')
         return
-
-    g = ctx.message.server
-
-    gid = g.id
-    membs = str(len(g.members))
-    membs_on = str(len([m for m in g.members if not m.status == Status.offline]))
-    users = str(len([m for m in g.members if not m.bot]))
-    users_on = str(len([m for m in g.members if not m.bot and not m.status == Status.offline]))
-    bots = str(len([m for m in g.members if m.bot]))
-    bots_on = str(len([m for m in g.members if m.bot and not m.status == Status.offline]))
-    created = str(g.created_at)
-    
-    em =Embed(title="membercount")
-    em.description =    "**\n" \
-                        "Members:   %s (%s)\n" \
-                        "  Users:   %s (%s)\n" \
-                        "  Bots:    %s (%s)\n" \
-                        "Created:   %s\n" \
-                        "**" % (membs, membs_on, users, users_on, bots, bots_on, created)
-
-    await client.send_message(ctx.message.channel, embed=em)
+    else:
+        if member.server_permissions.administrator == False:
+            await client.say('**You do not have permission to use this command**')
+            return
+        else:
+            embed=discord.Embed(title="Announcement", description="{}".format(msg), color = 0xf9fcfc)
+            await client.send_message(channel, embed=embed)
+            await client.delete_message(ctx.message)
 	
 client.run(os.getenv('Token'))
 

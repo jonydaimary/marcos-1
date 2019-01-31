741b8ab2d795d1b966b0dade945f3bfcf7681cf2
jonydaimary <45644556+jonydaimary@users.noreply.github.com>
2019-01-31 18:47:04 +0530

    Update code1.py

--- code1.py
+++ code1.py
@@ -30,6 +30,15 @@ async def on_ready():
 	return await client.change_presence(game=discord.Game(name='BETA VERSION', type=2)) #add_your_bot_status_here
 
 
+@client.command(pass_context = True)
+async def dc(ctx):
+    choices = ['https://media.giphy.com/media/uDPSXySAEDv56/giphy.gif', 'https://media.giphy.com/media/26vIg1DlkNdJr65q0/giphy.gif', 'https://media.giphy.com/media/jcIRoyJKQG3za/giphy.gif', 'https://media.giphy.com/media/26xBLVi4RuhYmV6zm/giphy.gif', 'https://media.giphy.com/media/xUOwGfcrlRjKjs2sSI/giphy.gif', 'https://media.giphy.com/media/l41Yq5KYEmbxFaeVq/giphy.gif', 'https://media.giphy.com/media/3o7abJW5ZuiByDelji/giphy.gif', 'https://media.giphy.com/media/xU67CtAMi8f5K/giphy.gif', 'https://media.giphy.com/media/VXQuKHDhTIBWM/giphy.gif']
+    embed=discord.Embed(title="Hello kryptonian... Here's your GIF...", color=0XFF69B4)
+    embed.set_image(url=random.choice(choices))
+    embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/516953091656908810/519072295080296469/Joker.png')
+    embed.set_footer(text=f'Requested by {ctx.message.author.name}', icon_url=f'{ctx.message.author.avatar_url}')
+    embed.timestamp = datetime.datetime.utcnow()
+    await client.send_message(ctx.message.channel, embed=embed)
 
 	
 client.run(os.getenv('Token'))

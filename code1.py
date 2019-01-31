f4c861b2bedea8d8f3b92ea5866df414f7e8ca74
jonydaimary <45644556+jonydaimary@users.noreply.github.com>
2019-01-31 21:19:08 +0530

    Update code1.py

--- code1.py
+++ code1.py
@@ -18,12 +18,11 @@ client = commands.Bot(description="marcos bot", command_prefix=commands.when_men
 
 @client.event
 async def on_ready():
-    print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
+    print('Logged in')
     print('--------')
     print('--------')
     print('Started New here ')
     print('Created by marcos')
-    client.loop.create_task(status_task())
 
 @client.command(pass_context = True)
 async def lovedetect(ctx, channel: discord.Channel=None, *, msg: str=None):

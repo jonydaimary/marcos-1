6047ccbe11f08cc0146095b8e698e81c99bf7749
jonydaimary <45644556+jonydaimary@users.noreply.github.com>
2019-01-31 19:07:25 +0530

    Update code1.py

--- code1.py
+++ code1.py
@@ -29,39 +29,7 @@ async def on_ready():
 	print('Started pubg') #add_your_bot_name_here
 	return await client.change_presence(game=discord.Game(name='BETA VERSION', type=2)) #add_your_bot_status_here
 
-
-@client.command(pass_context=True, aliases=['members'])
-@commands.has_permissions(kick_members=True)
-async def membercount(ctx, *args):
-    """
-    Shows stats and information about current guild.
-    ATTENTION: Please only use this on your own guilds or with explicit
-    permissions of the guilds administrators!
-    """
-    if ctx.message.channel.is_private:
-        await bot.delete_message(ctx.message)
-        return
-
-    g = ctx.message.server
-
-    gid = g.id
-    membs = str(len(g.members))
-    membs_on = str(len([m for m in g.members if not m.status == Status.offline]))
-    users = str(len([m for m in g.members if not m.bot]))
-    users_on = str(len([m for m in g.members if not m.bot and not m.status == Status.offline]))
-    bots = str(len([m for m in g.members if m.bot]))
-    bots_on = str(len([m for m in g.members if m.bot and not m.status == Status.offline]))
-    created = str(g.created_at)
-    
-    em =Embed(title="membercount")
-    em.description =    "**\n" \
-                        "Members:   %s \n" \
-                        "  Users:   %s \n" \
-                        "  Bots:    %s \n" \
-                        "Created:   %s\n" \
-                        "**" % (membs, membs_on, users, users_on, bots, bots_on, created)
-
-    await client.send_message(ctx.message.channel, embed=em)	 
+ 
 	
 	
 client.run(os.getenv('Token'))

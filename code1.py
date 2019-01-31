2e8b11a6bbf6194bca5a5838cc9cf15c48079755
jonydaimary <45644556+jonydaimary@users.noreply.github.com>
2019-01-31 18:04:47 +0530

    Update code1.py

--- code1.py
+++ code1.py
@@ -33,7 +33,7 @@ async def on_ready():
 @client.command(pass_context=True, aliases=["Help"])
 async def help(ctx):
     embed = discord.Embed(title="command Prefix: !! ", color=0Xf9fcfc)
-    embed.add_field(name="__**bot commands**__", value="`serverinfo` - This will show the server's information (Administrator). \n`poll` - Polling (Administrator). \n`clear<number>` - will clear messages(Administrator). \n`announce` - To announce the entered **#channel** n **matter** (Administrator). \n\n`botinfo` - Information about this BOT  \n`userinfo` - This will show the user's information. \n`lovedetect<@user1><@user2>` - This will show how the users love each other. \n`ping` - This will show your ping. \n`joke` - This will tell you a joke. ", inline=True)
+    embed.add_field(name="__**administrator commands**__", value="`serverinfo` - This will show the server's information (Administrator). \n`poll` - Polling (Administrator). \n`clear<number>` - will clear messages(Administrator). \n`announce` - To announce the entered **#channel** n **matter** (Administrator). \n\n`botinfo` - Information about this BOT  \n`userinfo` - This will show the user's information. \n`lovedetect<@user1><@user2>` - This will show how the users love each other. \n`ping` - This will show your ping. \n`joke` - This will tell you a joke. ", inline=True)
     embed.add_field(name="__**music commands**__", value="`play` - This will play the audio you want. \n`pause` - will pause the audio. \n`resume` - This will resume the audio. \n`skip` - will skip the music. \n`stop` -  will Bot disconnected. \n`song` - To Check The Current playing song. ") 
     embed.add_field(name="__**animals commands**__", value="`fox` - This will show a fox images. \n`dog` - This will show a dog images. \n`cat` - This will show a cat images. \n`bird` - This will show a bird images. ")
     embed.add_field(name="__**games commands**__", value="`rps` - This will play the rock, paper and scissors.\n`rolldice` - This will roll the dice and get 1 to 6 numbers. \n`flipcoin` - This will flip the coin. ")
@@ -41,7 +41,7 @@ async def help(ctx):
     embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/532751889172004865/540387622418251780/100.gif') 
     embed.set_footer(text=f"Requested by {ctx.message.author.name}", icon_url=f"{ctx.message.author.avatar_url}")
     embed.timestamp = datetime.datetime.utcnow()
-    await client.send_message(ctx.message.channel, embed=embed)   
+    await client.send_message(ctx.message.channel, embed=embed)     
 
 	
 client.run(os.getenv('Token'))

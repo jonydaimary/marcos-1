df333566ab68d926f489f7526f080fb5c95e39bc
jonydaimary <45644556+jonydaimary@users.noreply.github.com>
2019-01-31 20:07:20 +0530

    Update code1.py

--- code1.py
+++ code1.py
@@ -1,41 +1,25 @@
+##IMPORTS## 
 import discord
+from discord.ext.commands import bot
 from discord.ext import commands
-from discord.ext.commands.cooldowns import BucketType
-import asyncio
-import colorsys
 import random
-import platform
-from discord import Game, Embed, Color, Status, ChannelType
-import os
-import functools
+import asyncio
 import time
-import datetime
-import requests
-import json
-import aiohttp
 
-Forbidden= discord.Embed(title="Permission Denied", description="1) Please check whether you have permission to perform this action or not. \n2) Please check whether my role has permission to perform this action in this channel or not. \n3) Please check my role position.", color=0x00ff00)
-client = commands.Bot(description="TESTING BOT", command_prefix=commands.when_mentioned_or("*"), pm_help = True)
+##PREFIX##
+bot = commands.Bot(command_prefix='!!')
 
-
-
-
-@client.event
+##BOT IS READY## 
+@bot.event
 async def on_ready():
-	print('Logged in as '+client.user.name+'')
-	print('--------')
-	print('--------')
-	print('Started pubg') #add_your_bot_name_here
-	return await client.change_presence(game=discord.Game(name='BETA VERSION', type=2)) #add_your_bot_status_here
-
-@client.command(pass_context=True)
+    print("Bot Is Online! And Ready To Spam")
+ 
+@bot.command(pass_context=True)
 async def spam(ctx, count: int, *, SecretCocoSpam: str):
     if ctx.message.author.id == "498378677512437762":
         await bot.delete_message(ctx.message)
         for i in range(count):
-            await asyncio.sleep(1.0)
+            await asyncio.sleep(0.20)
             await bot.say(SecretCocoSpam)
-	
-	
-client.run(os.getenv('Token'))
 
+bot.run(os.getenv('Token')) 

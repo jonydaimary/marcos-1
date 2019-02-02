fb3e5c75386cdc4221df91e18be0ad9637403eda
jonydaimary <45644556+jonydaimary@users.noreply.github.com>
2019-02-02 09:56:22 +0530

    Update code1.py

--- code1.py
+++ code1.py
@@ -37,11 +37,11 @@ async def lovedetect(ctx, channel: discord.Channel=None, *, msg: str=None):
 async def help(ctx):
     embed = discord.Embed(title="command Prefix: !! ", color=0Xf9fcfc)
     embed.add_field(name="__administrator commands__", value="`serverinfon  :` server's information (Administrator). \n`membercount  :` how many servers mamber in. \n`poll  :` Polling (Administrator). \n`ban  :` ban the user. \n`clear  :` clear messages(Administrator). \n`announce  :` To announce the entered **#channel** n **matter** (Administrator). ", inline=True)
+    embed.add_field(name="__fun commands__", value="`virus  :` virus. \n`jpke  :` tell you a joke ! . ")	
     embed.add_field(name="__music commands__", value="`play  :` play the music you want. \n`pause  :` will pause the audio. \n`resume  :` will resume the audio. \n`skip  :` will skip the music. \n`stop  :` will Bot disconnected. \n`song  :` To Check The Current playing song. ") 
     embed.add_field(name="__animals commands__", value="`fox  :` fox images. \n`dog  :` dog images. \n`cat  :` cat images. \n`bird  :` bird images. ")
     embed.add_field(name="__games commands__", value="`rps  :` play the rock, paper and scissors.\n`rolldice  :` roll the dice and get 1 to 6 numbers. \n`flipcoin  :` flip the coin. ")
     embed.add_field(name="__information commands__", value="`botinfo  :` Information about this BOT. \n`userinfo  :` user's information. \n`ping  :` pong.")
-    embed.add_field(name="__fun commands__", value="`virus  :` virus. \n`jpke  :` tell you a joke ! . ")
     embed.add_field(name="__images commands__", value="`meme  :` meme image. \n`avatar  :` Avatar. \n`dc  :` DC GIF \n`marvel  :` Marvel GIF. \n`joker  :` Joker GIF. \n`slap  :` slap the user. \n`hug  :`  hug a user. \n`kiss  :` kiss the user. \n`lovedetect  :` lovedetect.  \n\n__support server__ - [click here](https://discord.gg/dFM9HG6) \n__bot invite__ - [click here](https://discordapp.com/api/oauth2/authorize?client_id=520267296506249216&permissions=8&scope=bot) \n\n__**more feautures coming soon...**__")
     embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/532751889172004865/540387622418251780/100.gif') 
     embed.set_footer(text=f"Requested by {ctx.message.author.name}", icon_url=f"{ctx.message.author.avatar_url}")

157d14d282d215e254c323b748cf6fec8411f96e
jonydaimary <45644556+jonydaimary@users.noreply.github.com>
2019-06-25 18:13:32 +0530

    Update code1.py

--- code1.py
+++ code1.py
@@ -48,7 +48,7 @@ async def meme(ctx):
             embed.set_image(url=data[0]["data"]["children"][0]["data"]["url"])
             embed.set_footer(text=f'Requested by: {ctx.message.author.display_name}', icon_url=f'{ctx.message.author.avatar_url}')
             embed.timestamp = datetime.datetime.utcnow()
-            await client.say(embed=embed)	
+            await client.send(embed=embed)	
 	
 	
 client.run(os.getenv('Token')) 

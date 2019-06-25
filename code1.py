2d6b26defe0101b792cfaf4cafbc4e4f153077fe
jonydaimary <45644556+jonydaimary@users.noreply.github.com>
2019-06-25 15:34:28 +0530

    Update code1.py

--- code1.py
+++ code1.py
@@ -14,18 +14,32 @@ import requests
 import json
 import aiohttp		
 
-client = commands.Bot(description="marcos bot", command_prefix=commands.when_mentioned_or("**"), pm_help = True)
+Forbidden= discord.Embed(title="Permission Denied", description="1) Please check whether you have permission to perform this action or not. \n2) Please check whether my role has permission to perform this action in this channel or not. \n3) Please check my role position.", color=0xf9fcfc)
+client = commands.Bot(description="marcos bot", command_prefix=commands.when_mentioned_or("!!"), pm_help = True)
 
 client.remove_command('help')
 
+async def status_task():
+    while True:
+        await client.change_presence(game=discord.Game(name='!!help | marcos.#0290', type=2)) 
+        await asyncio.sleep(120)
+        await client.change_presence(game=discord.Game(name='BETA VERSION')) 
+        await asyncio.sleep(120)
+        await client.change_presence(game=discord.Game(name='with ' +str(len(set(client.get_all_members())))+' users', type=3))
+        await asyncio.sleep(120)
+	
+	
+	
+	
 @client.event
 async def on_ready():
-    print('Logged in')
+    print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
     print('--------')
     print('--------')
     print('Started New here ')
     print('Created by marcos')
-
+    client.loop.create_task(status_task())
+	
 	
 
 @client.command(pass_context=True, aliases=["Help"])

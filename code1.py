ea8d34b30b31b61da8995365d4fc8e10f6d76853
jonydaimary <45644556+jonydaimary@users.noreply.github.com>
2019-06-25 18:34:22 +0530

    Update code1.py

--- code1.py
+++ code1.py
@@ -31,7 +31,7 @@ async def status_task():
 	
 @client.event
 async def on_ready():
-    print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
+    #print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
     print('--------')
     print('--------')
     print('Started New here ')

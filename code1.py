cd37b331ca79f894d35a317f73e72436f97baa54
jonydaimary <45644556+jonydaimary@users.noreply.github.com>
2019-06-25 15:38:28 +0530

    Update code1.py

--- code1.py
+++ code1.py
@@ -14,6 +14,7 @@ import requests
 import json
 import aiohttp		
 
+
 Forbidden= discord.Embed(title="Permission Denied", description="1) Please check whether you have permission to perform this action or not. \n2) Please check whether my role has permission to perform this action in this channel or not. \n3) Please check my role position.", color=0xf9fcfc)
 client = commands.Bot(description="marcos bot", command_prefix=commands.when_mentioned_or("!!"), pm_help = True)
 

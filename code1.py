b5a1c67ca28c52e0b3aab88f2389bd21e6ed83f6
jonydaimary <45644556+jonydaimary@users.noreply.github.com>
2019-06-25 19:35:52 +0530

    Update code1.py

--- code1.py
+++ code1.py
@@ -59,7 +59,7 @@ async def test(ctx):
 @client.command(pass_context = True)
 @commands.has_permissions(manage_messages = True)
 async def clear(ctx, number: int):
-  purge = await client.purge_from(ctx.message.channel, limit = number+1)	
+  purge = await ctx.purge_from(ctx.message.channel, limit = number+1)	
 
 	
 @client.command(pass_context=True, aliases=["Help"])

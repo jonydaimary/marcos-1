d134c36c5683ae046834c49eb056a34843153cc5
jonydaimary <45644556+jonydaimary@users.noreply.github.com>
2019-07-29 16:19:01 +0530

    Update code1.py

--- code1.py
+++ code1.py
@@ -24,6 +24,7 @@ client.remove_command('help')
 async def status_task():
     while True:
         await client.change_presence(status=discord.Status.idle)
+	await client.change_presence(game=discord.Game.gogo) 
 
 
 @client.event

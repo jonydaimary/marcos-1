d2bd7867a7d06f1b2462c6ceae3cc6523bb79cc5
jonydaimary <45644556+jonydaimary@users.noreply.github.com>
2019-08-15 11:24:53 +0530

    Update code1.py

--- code1.py
+++ code1.py
@@ -15,30 +15,9 @@ import requests
 import json
 import aiohttp		
 
-
-Forbidden= discord.Embed(title="Permission Denied", description="1) Please check whether you have permission to perform this action or not. \n2) Please check whether my role has permission to perform this action in this channel or not. \n3) Please check my role position.", color=0xf9fcfc)
 client = commands.Bot(description="cry n___ bot", command_prefix=commands.when_mentioned_or(">"), pm_help = True)
-
 client.remove_command('help')
 
-
-async def task():
-    while True:
-        channel = client.get_channel(605305731209494528)
-        address = "https://www.rrrather.com/botapi"
-        data = requests.get(address).json()
-        nsfw_check = data['nsfw']
-        if nsfw_check == False:
-            embed = discord.Embed(title=data['title'], description=f"**1){data['choicea']} \n \n2){data['choiceb']}** \n \n[Wanna know what others said about this question in www.rrrather.com? Click me...]({data['link']})", color=0xff69bf)
-            embed.set_author(name="It's question time folks...", icon_url=channel.guild.icon_url)
-            embed.set_footer(text=channel.guild.name)
-            embed.timestamp = datetime.datetime.utcnow()
-            await channel.send(embed=embed)
-            await asyncio.sleep(10)
-        elif nsfw_check == True:
-            await asyncio.sleep(1)	
-	
-	
 @client.event
 async def on_ready():
     print('--------')
